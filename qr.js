402a722ac80f7cd3f92ea40ff9bed9fb911bfa97
giftdee <discordbotz526@gmail.com>
2025-08-24 14:05:54 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -8,110 +8,86 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-	default: Mbuvi_Tech,
-	useMultiFileAuthState,
-	jidNormalizedUser,
-	Browsers,
-	delay,
-	makeInMemoryStore,
+        default: VENOM_XMD,
+        useMultiFileAuthState,
+        jidNormalizedUser,
+        Browsers,
+        delay,
+        makeInMemoryStore,
 } = require("@whiskeysockets/baileys");
 
 function removeFile(FilePath) {
-	if (!fs.existsSync(FilePath)) return false;
-	fs.rmSync(FilePath, {
-		recursive: true,
-		force: true
-	})
+        if (!fs.existsSync(FilePath)) return false;
+        fs.rmSync(FilePath, {
+                recursive: true,
+                force: true
+        })
 };
 const {
-	readFile
+        readFile
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
-	const id = makeid();
-	async function MBUVI_MD_QR_CODE() {
-		const {
-			state,
-			saveCreds
-		} = await useMultiFileAuthState('./temp/' + id)
-		try {
-			let Qr_Code_By_Mbuvi_Tech = Mbuvi_Tech({
-				auth: state,
-				printQRInTerminal: false,
-				logger: pino({
-					level: "silent"
-				}),
-				browser: Browsers.macOS("Desktop"),
-			});
+        const id = makeid();
+        async function VENOM_MD_QR_CODE() {
+                const {
+                        state,
+                        saveCreds
+                } = await useMultiFileAuthState('./temp/' + id)
+                try {
+                        let Qr_Code_By_VENOM_XMD = VENOM_XMD({
+                                auth: state,
+                                printQRInTerminal: false,
+                                logger: pino({
+                                        level: "silent"
+                                }),
+                                browser: Browsers.macOS("Desktop"),
+                        });
 
-			Qr_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Mbuvi_Tech.ev.on("connection.update", async (s) => {
-				const {
-					connection,
-					lastDisconnect,
-					qr
-				} = s;
-				if (qr) await res.end(await QRCode.toBuffer(qr));
-				if (connection == "open") {
-					await delay(5000);
-					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-					await delay(800);
-				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'Bellah~' + b64data });
-	
-				   let MBUVI_MD_TEXT = `
+                        Qr_Code_By_VENOM_XMD.ev.on('creds.update', saveCreds)
+                        Qr_Code_By_VENOM_XMD.ev.on("connection.update", async (s) => {
+                                const {
+                                        connection,
+                                        lastDisconnect,
+                                        qr
+                                } = s;
+                                if (qr) await res.end(await QRCode.toBuffer(qr));
+                                if (connection == "open") {
+                                        await delay(5000);
+                                        let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                                        await delay(800);
+                                   let b64data = Buffer.from(data).toString('base64');
+                                   let session = await Qr_Code_By_VENOM_XMD.sendMessage(Qr_Code_By_VENOM_XMD.user.id, { text: 'VENOM~' + b64data });
+
+                                   let VENOM_MD_TEXT = `
 ╔════════════════════◇
 ║『 SESSION CONNECTED』
-║ ✨VolTah-XMD🔷
-║ ✨Giddy Tennor 🔷
+║ ✨ VENOM-XMD 🔷
+║ ✨ Gifted Dave 🔷
 ╚════════════════════╝
-
 
 ---
 
 ╔════════════════════◇
-║『 YOU'VE CHOSEN VolTah XMD 』
-║ -Set the session ID in Heroku:
+║『 YOU'VE CHOSEN VENOM-XMD 』
+║ - Set the session ID in Heroku:
 ║ - SESSION_ID: 
 ╚════════════════════╝
+
 ╔════════════════════◇
-║ 『••• _V𝗶𝘀𝗶𝘁 𝗙𝗼𝗿_H𝗲𝗹𝗽 •••』
-║❍ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: youtube.com/@giddynokia
-║❍ 𝐎𝐰𝐧𝐞𝐫: 254756182478
-║❍ 𝐑𝐞𝐩𝐨: https://github.com/Tennor-modz/Bellah-Xmd 
-║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: https://chat.whatsapp.com/CzFlFQrkdzxFw0pxCBYM7H
-║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D
-║❍ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: _https://www.instagram.com/giddy_tennor_?igsh=YzZ0NDRoaXFxM2Zk
+║ 『••• VISIT FOR HELP •••』
+║ ❍ YouTube: youtube.com/@davlodavlo19
+║ ❍ Owner: 254104260236
+║ ❍ Repo: https://github.com/giftdee/VENOM-XMD 
+║ ❍ WhatsApp Group: https://chat.whatsapp.com/LfTFxkUQ1H7Eg2D0vR3n6g
+║ ❍ WhatsApp Channel: https://whatsapp.com/channel/0029VbApvFQ2Jl84lhONkc3k
+║ ❍ Instagram: https://www.instagram.com/gifted_dave_
 ║ ☬ ☬ ☬ ☬
 ╚═════════════════════╝
-𒂀 Enjoy VolTah MD
 
+𒂀 Enjoy VENOM-XMD
 
 ---
 
-Don't Forget To Give Star⭐ To My Repo
+Don't Forget To Give Star ⭐ To My Repo
 ______________________________`;
-	 await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id,{text:MBUVI_MD_TEXT},{quoted:session})
-
-
-
-					await delay(100);
-					await Qr_Code_By_Mbuvi_Tech.ws.close();
-					return await removeFile("temp/" + id);
-				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-					await delay(10000);
-					MBUVI_MD_QR_CODE();
-				}
-			});
-		} catch (err) {
-			if (!res.headersSent) {
-				await res.json({
-					code: "Service is Currently Unavailable"
-				});
-			}
-			console.log(err);
-			await removeFile("temp/" + id);
-		}
-	}
-	return await MBUVI_MD_QR_CODE()
-});
-module.exports = router
+         await Qr_Code_By_VENOM_XMD.sendMessage(Qr_Code_By_VENOM_XMD.user.id,{text:VENOM_MD_TEXT},{quoted:session})
