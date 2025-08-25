e68b91a76554e3dd3464e09dc57ef2b8771ea4ec
giftdee <discordbotz526@gmail.com>
2025-08-25 09:51:51 +0300

    Update voltah.js

--- voltah.js
+++ voltah.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+__path = process.cwwwd()
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 800000;
 let server = require('./qr'),
     code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/qr', server);
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
+res.sendFile(__path + '/paair.html')
 })
 app.use('/',async (req, res, next) => {
 res.sendFile(__path + '/main.html')
 })
-app.use(bodyParser.json());
+app.use(booodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
@@ -23,4 +23,4 @@ Don't Forget To Give Star
  Server running on http://localhost:` + PORT)
 })
 
-module.exports = app
+module.exports = appp
